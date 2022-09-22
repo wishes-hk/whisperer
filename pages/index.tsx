50ef64d031ed0dc308e89942ce9b006700ae3577
Martin Wong <kcwong395@gmail.com>
2022-09-22 16:49:59 -0400

    [feature]: integrate all the components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,35 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { Container } from '@mantine/core';
+import OverviewBanner from '../components/OverviewBanner/OverviewBanner';
+import { FooterWrapper } from '../components/FooterWrapper/FooterWrapper';
+import Hide from '../components/Hide/Hide';
+import { CookieBanner } from '../components/CookieBanner/CookieBanner';
+import Seek from '../components/Seek/Seek';
 
 export default function HomePage() {
+  const footerLinks = [
+    {
+      title: 'Direct Access', links: [{ link: '', label: 'Wishes Home' }, { link: '', label: 'HideNSeek' }],
+    },
+    {
+      title: 'Contact Us',
+      links: [
+        { link: 'https://kcwong395.github.io/', label: 'Team Members' },
+        { link: 'mailto:wisheshk.official@gmail.com', label: 'Project Collaboration' },
+        { link: '', label: 'Technical Support' },
+        { link: 'https://github.com/wishes-hk', label: 'Wishes Github' },
+      ],
+    },
+  ];
+
   return (
     <>
-      <Welcome />
-      <ColorSchemeToggle />
+      <Container size="lg" style={{ textAlign: 'center' }}>
+        <OverviewBanner />
+        <Hide />
+        <Seek />
+      </Container>
+      <CookieBanner />
+      <FooterWrapper data={footerLinks} />
     </>
   );
 }
