f7e3e277065729f02ebfb9c487b3c09402270764
Martin Wong <kcwong395@gmail.com>
2022-09-22 16:42:32 -0400

    [feature]: add overview banner

---
+++ components/OverviewBanner/OverviewBanner.tsx
@@ -0,0 +1,26 @@
+export default function OverviewBanner() {
+  return (
+    <pre>
+            <code style={{ fontSize: '8px' }}>{`
+                                           dddddddd                                                                                                                         
+HHHHHHHHH     HHHHHHHHH  iiii              d::::::d                    NNNNNNNN        NNNNNNNN   SSSSSSSSSSSSSSS                                        kkkkkkkk           
+H:::::::H     H:::::::H i::::i             d::::::d                    N:::::::N       N::::::N SS:::::::::::::::S                                       k::::::k           
+H:::::::H     H:::::::H  iiii              d::::::d                    N::::::::N      N::::::NS:::::SSSSSS::::::S                                       k::::::k           
+HH::::::H     H::::::HH                    d:::::d                     N:::::::::N     N::::::NS:::::S     SSSSSSS                                       k::::::k           
+  H:::::H     H:::::H  iiiiiii     ddddddddd:::::d     eeeeeeeeeeee    N::::::::::N    N::::::NS:::::S                eeeeeeeeeeee        eeeeeeeeeeee    k:::::k    kkkkkkk
+  H:::::H     H:::::H  i:::::i   dd::::::::::::::d   ee::::::::::::ee  N:::::::::::N   N::::::NS:::::S              ee::::::::::::ee    ee::::::::::::ee  k:::::k   k:::::k 
+  H::::::HHHHH::::::H   i::::i  d::::::::::::::::d  e::::::eeeee:::::eeN:::::::N::::N  N::::::N S::::SSSS          e::::::eeeee:::::ee e::::::eeeee:::::eek:::::k  k:::::k  
+  H:::::::::::::::::H   i::::i d:::::::ddddd:::::d e::::::e     e:::::eN::::::N N::::N N::::::N  SS::::::SSSSS    e::::::e     e:::::ee::::::e     e:::::ek:::::k k:::::k   
+  H:::::::::::::::::H   i::::i d::::::d    d:::::d e:::::::eeeee::::::eN::::::N  N::::N:::::::N    SSS::::::::SS  e:::::::eeeee::::::ee:::::::eeeee::::::ek::::::k:::::k    
+  H::::::HHHHH::::::H   i::::i d:::::d     d:::::d e:::::::::::::::::e N::::::N   N:::::::::::N       SSSSSS::::S e:::::::::::::::::e e:::::::::::::::::e k:::::::::::k     
+  H:::::H     H:::::H   i::::i d:::::d     d:::::d e::::::eeeeeeeeeee  N::::::N    N::::::::::N            S:::::Se::::::eeeeeeeeeee  e::::::eeeeeeeeeee  k:::::::::::k     
+  H:::::H     H:::::H   i::::i d:::::d     d:::::d e:::::::e           N::::::N     N:::::::::N            S:::::Se:::::::e           e:::::::e           k::::::k:::::k    
+HH::::::H     H::::::HHi::::::id::::::ddddd::::::dde::::::::e          N::::::N      N::::::::NSSSSSSS     S:::::Se::::::::e          e::::::::e         k::::::k k:::::k   
+H:::::::H     H:::::::Hi::::::i d:::::::::::::::::d e::::::::eeeeeeee  N::::::N       N:::::::NS::::::SSSSSS:::::S e::::::::eeeeeeee   e::::::::eeeeeeee k::::::k  k:::::k  
+H:::::::H     H:::::::Hi::::::i  d:::::::::ddd::::d  ee:::::::::::::e  N::::::N        N::::::NS:::::::::::::::SS   ee:::::::::::::e    ee:::::::::::::e k::::::k   k:::::k 
+HHHHHHHHH     HHHHHHHHHiiiiiiii   ddddddddd   ddddd    eeeeeeeeeeeeee  NNNNNNNN         NNNNNNN SSSSSSSSSSSSSSS       eeeeeeeeeeeeee      eeeeeeeeeeeeee kkkkkkkk    kkkkkkk
+           `}
+            </code>
+    </pre>
+  );
+}
